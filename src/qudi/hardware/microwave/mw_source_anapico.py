f47b6a003404d7a834f1b41a92769f0f020ce5ba
MagnusBenke <magnus.benke@pi3.uni-stuttgart.de>
2024-02-06 14:08:49 +0100

    Bug Fixes for The Hardware Connection

--- src/qudi/hardware/microwave/mw_source_anapico.py
+++ src/qudi/hardware/microwave/mw_source_anapico.py
@@ -1,7 +1,7 @@
 try:
     import pyvisa as visa
 except ImportError:
-    import visa
+    Warning('Pyvisa not found!')
 import time
 from typing import Any, Callable, Mapping, Optional
 import numpy as np
@@ -39,11 +39,19 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
         
         # Generate constraints
         if self._model == 'APSIN3000-HC':
-            freq_limits = (9e3, 3.3e6)
+            freq_limits = (9e3, 3.3e9)
             power_limits = (-30, 13)
-            scan_size_limits = (2,1000) # The pyvisa throws an error if the size is to large.... despite the actual hardawre limitation
+            scan_size_limits = (2,1000) # The pyvisa throws an error if the size is to large.... despite the actual hardware limitation
+        elif self._model == 'APSIN6010':
+            freq_limits = (9e3, 6.1e9)
+            power_limits = (-30, 18)
+            scan_size_limits = (2, 20000)
+        elif self._model == 'APSIN4010':
+            freq_limits = (9e3, 4.1e9)
+            power_limits = (-30, 18)
+            scan_size_limits = (2, 20000)
         else:
-            freq_limits = (9e3, 3.3e6)
+            freq_limits = (9e3, 3.3e9)
             power_limits = (-30, 13)
             scan_size_limits = (2,1000)
             self.log.warning('Model string unknown, hardware constraints might be wrong.')
@@ -56,7 +64,6 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
             sample_rate_limits=(0.1,100) 
         )
 
-        # TODO set all activation vals ..... why again?
         
         
     def on_deactivate(self):
@@ -128,7 +135,18 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
         """
         with self._thread_lock:
             return self._scan_frequencies
-        
+
+    @property
+    def scan_sample_rate(self):
+        """Read-only property returning the currently configured scan sample rate in Hz.
+
+        @return float: The currently set scan sample rate in Hz
+        """
+        with self._thread_lock:
+            return Warning('NO sample rate available')
+
+
+
     @property
     def scan_mode(self):
         """Scan mode Enum. Must implement setter as well.
@@ -155,7 +173,7 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
             self._command_wait(f':FREQ:FIX {frequency:e}')
             self._command_wait(f':AMPL {power:f}')
 
-    def configure_scan(self, frequencies, power, mode):
+    def configure_scan(self, power, frequencies, mode,sample_rate):
         """"Configure the Sweep microwave output. Does not start physical signal
         output, see also "scan_on". 
         TODO check if really not turning on auto
@@ -166,6 +184,7 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
             Note power can also be sweeped but not implemented so far
         @param mode: mode to performe, either SamplingOutputMode.JUMP_LIST or 
             SamplingOutputMode.EQUIDISTANT_SWEEP
+        @sample_rate: No sample input possible, needs to be given anyway because of programming
         """
         with self._thread_lock:
             if self.module_state() != 'idle':
@@ -173,6 +192,7 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
             self._assert_scan_configuration_args(power,frequencies,mode,sample_rate=1) # Dummy sampling rate is used, as the devices do not have this feature
             self._scan_mode = mode
             self._scan_power = power
+            self._scan_sample_rate = sample_rate
 
             if mode == SamplingOutputMode.JUMP_LIST:
                 self._scan_frequencies = np.asarray(frequencies, dtype=np.float64)
@@ -180,6 +200,7 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
             elif mode == SamplingOutputMode.EQUIDISTANT_SWEEP:
                 self._scan_frequencies = tuple(frequencies)
                 self._write_sweep()
+
 
     def cw_on(self):
         """ Switches on cw microwave output.
@@ -200,7 +221,7 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
                 time.sleep(0.2)
             self.module_state.lock()
 
-    def scan_on(self):
+    def start_scan(self):
         """Switches on the microwave scanning.
 
         Must return AFTER the output is actually active (and can receive triggers for example).
@@ -223,7 +244,7 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
                 self._command_wait(':OUTP:STAT ON')
             self.module_state.lock()
 
-    def scan_reset(self):
+    def reset_scan(self):
          """Reset currently running scan and return to start frequency.
         Does not need to stop and restart the microwave output if the device allows soft scan reset.
         """
@@ -232,8 +253,8 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
                 return
             if self._in_cw_mode():
                 raise RuntimeError('Can not reset frequency scan. CW microwave output active.')
-            self._command_wait(':ABOR')
-
+            #self._command_wait(':ABOR')
+            print('abort ommited')
 
     def off(self):
         """Switches off any microwave output (both scan and CW).
@@ -245,10 +266,8 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
                 mode = self._in_cw_mode()
                 if not mode:
                     self._command_wait(':FREQ:MODE FIX')
-
                 self._device.write(':OUTP OFF')
-                while int(float(self._device.query(':OUTP STAT?'))) != 0:
-                    time.sleep(0.2)
+                time.sleep(1) # ":OUTP STAT?" returns empty for some reason... just hope after a waiting time the output is off
                 self.module_state.unlock()
 
     def _command_wait(self, command_str):
@@ -276,19 +295,24 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
     
     def _write_list(self):
         # Front panel only updates when in CW mode
-        if not self._in_cw_mode():
+        if not self._in_list_mode():
             self._command_wait(':FREQ:MODE FIX')
-
-        freq_str = ', '.join(f'{freq:f}' for freq in self._scan_frequencies)
+        freq_str = ','.join(f'{freq:f}' for freq in self._scan_frequencies)
         self._command_wait(f':LIST:FREQ {freq_str}')
-
-        self._command_wait(f':LIST:POW {self.scan_power:f}')
-
+        self._command_wait(f':LIST:POW {self._scan_power:f}')
+        self._command_wait(':TRIG:SOUR EXT')
+        self._command_wait(':SWE:DWEL 0.00005') # Set to minimal to not interfere with the trigger 
+        self._command_wait(':SWE:DEL 0') # 
+        
         if not self._in_list_mode():
             self._command_wait(':FREQ:MODE LIST')
 
+        
     def _write_sweep(self):
         # Front panel only updates when in CW mode
+        if self._model == 'APSIN6010':
+            # This device needs to be once in the sweep mode to accept any frequency changes
+            self._command_wait(':FREQ:MODE SWE')
         if not self._in_cw_mode():
             self._command_wait(':FREQ:MODE FIX')
 
@@ -302,5 +326,14 @@ class MicrowaveAnaPicoAPSin(MicrowaveInterface):
         self._device.write(f':POW {self._scan_power:f}')
 
         self._command_wait(':TRIG:SOUR EXT')
+        if self._model == 'APSIN6010':
+            # This device uses internal trigger aswell even if set to ext 
+            dwel = 1/self._scan_sample_rate
+            self._command_wait(f':SWE:DWEL {dwel:f}') 
+        else:
+            self._command_wait(':SWE:DWEL 0.00005') # Set to minimal to not interfere with the trigger 
+        self._command_wait(':SWE:DEL 0') 
+        self._command_wait(':TRIG:TYPE NORM')
+        self._command_wait(':TRIG:SLOP POS')
 
         self._command_wait(':FREQ:MODE SWE')
